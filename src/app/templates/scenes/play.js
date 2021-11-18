1fa9f2bd68dd584dff7686e9f7a47f1114c2cebd
mrwipe <>
2021-11-18 16:24:39 +0300

    fixed position

--- src/app/templates/scenes/play.js
+++ src/app/templates/scenes/play.js
@@ -6,23 +6,29 @@ class AppTemplatesScenesPlay {
             {
                 type: Urso.types.objects.GRAPHICS,
                 name: 'graphics rectangle example',
+                alignX: 'center',
+                alignY: 'center',
+                x: -500,
+                y: -200,
                 figure: {
-                    rectangle: [200, 200, 200, 200],
+                    rectangle: [0, 0, 200, 200],
                     fillColor: 0xFFFFFF
                 }
             },
             {
                 type: Urso.types.objects.GRAPHICS,
                 name: 'graphics polygon example',
+                alignX: 'center',
+                alignY: 'center',
                 figure: {
                     polygon: [
-                        500, 400, 
-                        700, 200,
-                        900, 200,
-                        1100, 400,
-                        900, 600,
-                        700, 600,
-                        500, 400
+                        0, -100, 
+                        200, -300,
+                        400, -300,
+                        600, -100,
+                        400, 100,
+                        200, 100,
+                        0, -100
                     ],
                     fillColor: 0x0000FFF
                 }
