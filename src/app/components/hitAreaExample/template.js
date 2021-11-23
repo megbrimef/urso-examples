a10f05c0d7d1e39fd110ea84ff51c88cebe1ac8f
mrwipe <>
2021-11-23 11:55:46 +0300

    added text over hitarea

--- src/app/components/hitAreaExample/template.js
+++ src/app/components/hitAreaExample/template.js
@@ -3,6 +3,93 @@ class ComponentsHitAreaExampleTemplate {
 
         this.objects = [
             {
+                type: Urso.types.objects.CONTAINER,
+                x: -500, y: 200,
+                contents: [
+                    {
+                        type: Urso.types.objects.HITAREA,
+                        name: 'hitArea1',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        x: -140,
+                        y: -40,
+                        width: 300,
+                        height: 100
+                    },
+                    {
+                        type: Urso.types.objects.TEXT,
+                        name: 'hitAreaText_1',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        fontSize: 60,
+                        fill: 'white',
+                        text: 'press left'
+                    },
+                ]
+            },
+            {
+                type: Urso.types.objects.CONTAINER,
+                x: 0, y: 200,
+                contents: [
+                    {
+                        type: Urso.types.objects.HITAREA,
+                        name: 'hitArea2',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        x: -170,
+                        y: -40,
+                        width: 350,
+                        height: 100
+                    },
+                    {
+                        type: Urso.types.objects.TEXT,
+                        name: 'hitAreaText_2',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        fontSize: 60,
+                        fill: 'white',
+                        text: 'press center'
+                    },
+                ]
+            },
+            {
+                type: Urso.types.objects.CONTAINER,
+                x: 500, y: 200,
+                contents: [
+                    {
+                        type: Urso.types.objects.HITAREA,
+                        name: 'hitArea3',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        x: -140,
+                        y: -40,
+                        width: 300,
+                        height: 100
+                    },
+                    {
+                        type: Urso.types.objects.TEXT,
+                        name: 'hitAreaText_3',
+                        alignX: 'center',
+                        alignY: 'center',
+                        anchorX: 0.5,
+                        anchorY: 0.5,
+                        fontSize: 60,
+                        fill: 'white',
+                        text: 'press right'
+                    },
+                ]
+            },
+            {
                 type: Urso.types.objects.TEXT,
                 name: 'exampleText',
                 alignX: 'center',
@@ -10,45 +97,10 @@ class ComponentsHitAreaExampleTemplate {
                 anchorX: 0.5,
                 anchorY: 0.5,
                 fontSize: 100,
+                y: -100,
                 fill: 'white',
                 text: 'press any hitArea below'
             },
-            {
-                type: Urso.types.objects.HITAREA,
-                name: 'hitArea1',
-                alignX: 'center',
-                alignY: 'center',
-                anchorX: 0.5,
-                anchorY: 0.5,
-                x: -475, y: 200,
-                alpha: 1,
-                width: 150,
-                height: 150
-            },
-            {
-                type: Urso.types.objects.HITAREA,
-                name: 'hitArea2',
-                alignX: 'center',
-                alignY: 'center',
-                anchorX: 0.5,
-                anchorY: 0.5,
-                x: -75, y: 200,
-                alpha: 1,
-                width: 150,
-                height: 150
-            },
-            {
-                type: Urso.types.objects.HITAREA,
-                name: 'hitArea3',
-                alignX: 'center',
-                alignY: 'center',
-                anchorX: 0.5,
-                anchorY: 0.5,
-                x: 325, y: 200,
-                alpha: 1,
-                width: 150,
-                height: 150
-            }
         ];
     }
 }
