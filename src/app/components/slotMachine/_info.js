34ab529e9746c4eeef249ed99d5e75c8b1416864
mrwipe <>
2021-11-29 13:34:15 +0300

    slotMachine _info fix

--- src/app/components/slotMachine/_info.js
+++ src/app/components/slotMachine/_info.js
@@ -1,7 +1,4 @@
 Urso.App.Components.SlotMachine = {
-    Config: require('./config.js'),
     Template: require('./template.js'),
     SymbolsConfig: require('./symbolsConfig.js'),
-
-    //FIXME: Maybe move it to modes and make separation for config // DONE: it was decided not to use modes
 };
