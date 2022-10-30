9e4bed794f76c04bf5b9cdd2adc0d6f5fbebbe75
megbrimef <megbrimef@gmail.com>
2022-10-30 15:18:34 +0200

    [config] speed update

--- src/app/components/slotMachine/config.js
+++ src/app/components/slotMachine/config.js
@@ -8,7 +8,8 @@ class AppComponentsSlotMachineConfig extends Urso.SlotBase.Components.SlotMachin
             bottom: { to: { y: 80, x: 0 }, duration: 200 }
         };
 
-        basicConfig.speedUpReelsFactor = 10;
+        basicConfig.speedUpReelsFactor = 3;
+        basicConfig.symbolSpeed = 3;
 
         return basicConfig;
     }
